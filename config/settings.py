ef6abe1a6c5bfdba46999ce517f06aecf266cfaa
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-11-11 12:09:51 +0500

    User model done

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
+    'rest_framework',
+
+    # local apps
+    'users',
+    'shared',
 ]
 
 MIDDLEWARE = [
@@ -109,8 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
